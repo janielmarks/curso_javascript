889b968e5d389955c8d13e0a37f46598851f5006
Janiel Ferreira Marques <janielmarks@MacBook-Pro-de-Janiel.local>
2023-09-19 17:06:07 -0300

    exercicio da aula 09

---
+++ aula-09.js
@@ -0,0 +1,12 @@
+function soma(item1, item2) {
+    return item1 + item2; 
+}
+
+function diminuir(item1, item2) {
+    return item1 - item2;
+
+}
+
+console.log('soma: ', soma(5, 6)); 
+console.log('soma: ', soma(100, 200)); 
+console.log('soma: ', diminuir(5, 6, )); 
