afed6d30ccec7b43fb30b289944ec26f2e51d0ea
Janiel Ferreira Marques <janielmarks@MacBook-Pro-de-Janiel.local>
2023-09-19 16:43:54 -0300

    exercicios da aula 07

---
+++ aula-07.js
@@ -0,0 +1,12 @@
+const idadeJulio = 18;
+const idadeAndressa = 23;
+const idadeJose = 14;
+
+const idades = [18, 23, 14,18, 23, 14,18, 23, 14,18, 23, 14,18, 23, 14];
+for(let i = 0; i < idades.lenght; i++) {
+
+    console.log('resultado', idades.lenght)
+}
+
+
+
