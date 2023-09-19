2c2e1cccb453cf8a2e2debee4fd6dc09ce7de221
Janiel Ferreira Marques <janielmarks@MacBook-Pro-de-Janiel.local>
2023-09-19 14:48:32 -0300

    exemplos da aula 03

---
+++ aula-03.js
@@ -0,0 +1,11 @@
+const idade = 28; 
+let podeEntrar = false;
+const nome = 'julio';
+
+if(idade >= 18 && nome === 'julio') {
+   console.log('entrou!')
+}
+else {
+    console.log ('não entra!')
+}
+ 
